59a863e7b2c3abae83b88b1299a2d0f4e8f59a39
Ziabeher Phillips <zabq02@gmail.com>
2022-03-27 18:02:17 -0400

    navigation UI

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Navigation.css'
+import { BsCloudHaze2Fill } from 'react-icons/bs'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div className="nav-container">
+    <nav className='main-container'>
+        <div className="logo">
+            <BsCloudHaze2Fill className='cloud-logo'/>
+            <h1>CloudTech</h1>
+        </div>
+
+        <ul className="nav-links">
+            <li className='nav-item'><a href="" className="nav-link">Home</a></li>
+            <li className='nav-item'><a href="" className="nav-link">In Stock</a></li>
+            <li className='nav-item'><a href="" className="nav-link">Contact</a></li>
+        </ul>
+
+        <div className="nav-menu">
+            <div className="menu-line"></div>
+            <div className="menu-line"></div>
+            <div className="menu-line"></div>
+        </div>
+    </nav>
+    </div>
   )
 }
 
