d52c93474a6cd7299ce5d03308065605179ec20f
Ziabeher Phillips <zabq02@gmail.com>
2022-03-28 14:40:41 -0400

    showcase1 section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero";
 import Navigation from "./components/Navigation/Navigation";
+import Showcase1 from "./components/Showcase1/Showcase1";
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
       <Hero/>
+      <Showcase1/>
     </div>
   );
 }
---
+++ src/components/Showcase1/Showcase1.jsx
@@ -0,0 +1,23 @@
+import './Showcase1.css'
+import React from 'react'
+import IMG from '../../assets/showcase1.png'
+
+const Showcase1 = () => {
+  return (
+    <div className="showcase1">
+        <h1>The Future of Technology is here.</h1>
+
+        <div className="s1-flex">
+            <div className="s1-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac justo at ligula venenatis pharetra. Nulla molestie vitae lorem at laoreet. Nam justo lectus, sodales et massa sagittis, viverra vulputate</p>
+            </div>
+
+            <div className="s1-img">
+                <img src={IMG} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Showcase1
